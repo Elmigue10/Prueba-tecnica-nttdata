3d466c4f446d0ab32e6ef7ddbebf5b8d8f52e624
Elmigue10 <miguelvalbuena1026@gmail.com>
2022-06-15 11:11:47 -0500

    Documentación con swagger

--- ms-afiliados/src/main/java/com/nttdata/msafiliados/business/controller/AfiliadoController.java
+++ ms-afiliados/src/main/java/com/nttdata/msafiliados/business/controller/AfiliadoController.java
@@ -2,8 +2,9 @@ package com.nttdata.msafiliados.business.controller;
 
 import com.nttdata.msafiliados.business.service.AfiliadoService;
 import com.nttdata.msafiliados.domain.dto.AfiliadoDto;
-import com.nttdata.msafiliados.domain.entity.Afiliado;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,45 +20,53 @@ public class AfiliadoController {
         this.afiliadoService = afiliadoService;
     }
 
-    @GetMapping("")
+    @ApiOperation(value = "Listar todos los afiliados")
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AfiliadoDto>> getAll(){
         return new ResponseEntity<>(afiliadoService.getAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @ApiOperation(value = "Buscar afiliado por id")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AfiliadoDto> findById(@PathVariable Integer id){
         return new ResponseEntity<>(afiliadoService.findById(id), HttpStatus.OK);
     }
 
-    @GetMapping("/filtrarIdentificacion")
+    @ApiOperation("Filtrar busqueda por numero de identificacion")
+    @GetMapping(value = "/filtrarIdentificacion", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AfiliadoDto> findByNumeroIdentificacion(@RequestParam String numeroIdentificacion){
         return new ResponseEntity<>(afiliadoService.findByNumeroIdentificacion(numeroIdentificacion), HttpStatus.OK);
     }
 
-    @GetMapping("/filtrarUsuarioCreacion")
+    @ApiOperation("Filtrar busqueda por usuario de creacion")
+    @GetMapping(value = "/filtrarUsuarioCreacion", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AfiliadoDto>> filtrar(@RequestParam String usuarioCreacion){
         return new ResponseEntity<>(afiliadoService.findByUsuarioCreacion(usuarioCreacion), HttpStatus.OK);
     }
 
-    @GetMapping("/filtrarFechaCreacion")
+    @ApiOperation(value = "Filtrar busqueda por intervalo de fecha de creacion")
+    @GetMapping(value = "/filtrarFechaCreacion", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AfiliadoDto>> filtrarFechaCreacion(@RequestParam String fromDate,
                                                                @RequestParam String toDate){
         return new ResponseEntity<>(afiliadoService.filterByFechaCreacion(fromDate, toDate), HttpStatus.OK);
     }
 
-    @PostMapping("")
+    @ApiOperation(value = "Guardar un nuevo afiliado")
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> saveAfiliado(@RequestBody AfiliadoDto afiliadoDto){
         afiliadoService.saveAfiliado(afiliadoDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PutMapping("")
+    @ApiOperation(value = "Actualizar un afiliado")
+    @PutMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> updatedAfiliado(@RequestBody AfiliadoDto afiliadoDto){
         afiliadoService.updateAfiliado(afiliadoDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Eliminar un afiliado")
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> deleteAfiliadoById(@PathVariable Integer id){
         afiliadoService.deleteAfiliadoById(id);
         return new ResponseEntity<>(HttpStatus.OK);
